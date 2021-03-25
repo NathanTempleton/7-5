3c9c7a818eb4aef62890e91dd0500d46551c3b6b
NathanTempleton <nathan.templeton@ocsbstudent.ca>
2021-03-25 13:37:40 +0000

    Fixed errors + added style

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ function calculator () {
   age = parseInt(age)
   day = document.getElementById('day-input').value
 
-  if ((age < 18) && (day !='weekend')) {
+  if ((age < 18) && (day !== 'weekend')) {
     document.getElementById('result').innerHTML = 'You have to go to school today.'
-    } else if ((age > 18) && (day !='weekend')) {
-    document.getElementById('result').innerHTML = 'You have to go to work.'
+  } else if ((age > 18) && (day !== 'weekend')) {
+  document.getElementById('result').innerHTML = 'You have to go to work.'
   } else {
     document.getElementById('result').innerHTML = 'You get to relax for the weekend.'
   } document.getElementById('result').style.display = 'block'
